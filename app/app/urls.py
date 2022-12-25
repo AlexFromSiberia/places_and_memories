130a398db5af47bdb4883f537528527401a15450
AlexFromSiberia <id764g@gmail.com>
2022-12-25 14:16:59 +0700

    - An issue solved: photos didn't save to a memory entry. Attr 'enctype="multipart/form-data' was added to update/add forms in html files.
    - class 'LatLngPopup' from folium lib was modified and moved to a separate file 'addition_funcs.py'.
    
    Modification: two strings were added:
    1. ' parent.document.getElementById("id_lng").value = e.latlng.lng.toFixed(4);'
    2. 'parent.document.getElementById("id_lat").value = e.latlng.lat.toFixed(4);'
    
    The modification allows to save coordinates of the place chosen on the map.

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+# чтобы иметь доступ к переменным MEDIA_URL, MEDIA_ROOT
+from django.conf import settings
+# static module for MEDIA_URL, MEDIA_ROOT
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,3 +15,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/main/views.py
+++ app/main/views.py
@@ -9,13 +9,11 @@ from django.contrib.auth.decorators import login_required
 from .models import Memory
 from django.views.generic.list import ListView
 from django.views.generic import DetailView, UpdateView, DeleteView, CreateView
-
 from django.http import Http404, HttpResponseRedirect
 import folium
+from .addition_funcs import LatLngPopupModified
 
-
-from branca.element import MacroElement           # delete it
-from jinja2 import Template                       # delete it
+folium.LatLngPopup = LatLngPopupModified
 
 
 def index(request):
@@ -39,44 +37,6 @@ class Memories(DetailView):
     template_name = 'main/detail_entry.html'
     context_object_name = "place"
     # allow_empty = False
-
-
-# class LatLngPopup(MacroElement):
-#     """
-#     When one clicks on a Map that contains a LatLngPopup,
-#     a popup is shown that displays the latitude and longitude of the pointer.
-#     """
-#     _template = Template(u"""
-#             {% macro script(this, kwargs) %}
-#                 var {{this.get_name()}} = L.popup();
-#                 function latLngPop(e) {
-#                     {{this.get_name()}}
-#                         .setLatLng(e.latlng)
-#                         .setContent("Latitude: " + e.latlng.lat.toFixed(4) +
-#                                     "<br>Longitude: " + e.latlng.lng.toFixed(4))
-#                         .openOn({{this._parent.get_name()}});
-#                     parent.document.getElementById("id_lng").value = e.latlng.lng.toFixed(4);
-#                     parent.document.getElementById("id_lat").value = e.latlng.lat.toFixed(4);
-#                     }
-#                 {{this._parent.get_name()}}.on('click', latLngPop);
-#             {% endmacro %}
-#             """)  # noqa
-#
-#     def __init__(self):
-#         super(LatLngPopup, self).__init__()
-#         self._name = 'LatLngPopup'
-
-
-class MemoryUpdate(SuccessMessageMixin, UpdateView):
-    """Page: Update memory (for memory owner only)"""
-    model = Memory
-    form_class = MemoryForm
-    template_name = 'main/update_entry.html'
-    context_object_name = "place"
-    allow_empty = False
-    success_url = reverse_lazy("main:places")
-    success_message = 'Memory has been successfully updated!'
-
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -108,35 +68,46 @@ class MemoryUpdate(SuccessMessageMixin, UpdateView):
         return context
 
 
-# class MemoryAdd(SuccessMessageMixin, CreateView, PermissionRequiredMixin):
-#     """Page: Add new memories """
-#     #permission_required = 'news.can_delete_news_article'
-#
-#     model = Memory
-#     form_class = MemoryForm
-#     template_name = 'main/add_entry.html'
-#     success_url = reverse_lazy("main:places")
-#     success_message = f'Memory about the place has been successfully added!'
-#     context_object_name = "place"
-#
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         latitude = 0
-#         longitude = 0
-#         m = folium.Map([latitude, longitude], zoom_start=5)
-#         test = folium.Html(f'<strong>New place</strong>', script=True)
-#         popup = folium.Popup(test, max_width=2650)
-#         folium.RegularPolygonMarker(location=[latitude, longitude],
-#                                     popup=popup,
-#                                     tooltip=f"Click me ").add_to(m)
-#         folium.LatLngPopup().add_to(m)
-#         m = m._repr_html_()
-#         context['map'] = m
-#         return context
+class MemoryUpdate(SuccessMessageMixin, UpdateView):
+    """Page: Update memory (for memory owner only)"""
+    model = Memory
+    form_class = MemoryForm
+    template_name = 'main/update_entry.html'
+    context_object_name = "place"
+    # allow_empty = False
+    success_url = reverse_lazy("main:places")
+    success_message = 'Memory has been successfully updated!'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # getting coordinates for already existing point
+        if self.object.latitude and self.object.longitude:
+            latitude = self.object.latitude
+            longitude = self.object.longitude
+        else:
+            latitude = 0
+            longitude = 0
+        # Getting place name
+        if self.object.place:
+            place = self.object.place
+
+        # Getting place name
+        if self.object.text:
+            text = self.object.text
+
+        m = folium.Map([latitude, longitude], zoom_start=5)
+        test = folium.Html(f'<strong>{place}</strong>', script=True)
+        popup = folium.Popup(test, max_width=2650)
+        folium.RegularPolygonMarker(location=[latitude, longitude],
+                                    popup=popup,
+                                    tooltip=f"Click me to get place name ! Full memory: {text}").add_to(m)
+        folium.LatLngPopup().add_to(m)
+        m = m._repr_html_()
+        context['map'] = m
+        return context
 
 
 def add_memory(request):
-
     if request.method == 'POST':
         form = MemoryForm(request.POST, request.FILES)
         if form.is_valid():
@@ -163,12 +134,6 @@ def add_memory(request):
     return render(request, 'main/add_entry.html', context)
 
 
-
-
-
-
-
-
 class MemoryDelete(SuccessMessageMixin, DeleteView, PermissionRequiredMixin):
     """Page: Delete memories """
     #permission_required = 'news.can_delete_news_article'
@@ -180,5 +145,30 @@ class MemoryDelete(SuccessMessageMixin, DeleteView, PermissionRequiredMixin):
     context_object_name = "place"
 
 
+# class MemoryAdd(SuccessMessageMixin, CreateView, PermissionRequiredMixin):
+#     """Page: Add new memories """
+#     #permission_required = 'news.can_delete_news_article'
+#
+#     model = Memory
+#     form_class = MemoryForm
+#     template_name = 'main/add_entry.html'
+#     success_url = reverse_lazy("main:places")
+#     success_message = f'Memory about the place has been successfully added!'
+#     context_object_name = "place"
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         latitude = 0
+#         longitude = 0
+#         m = folium.Map([latitude, longitude], zoom_start=5)
+#         test = folium.Html(f'<strong>New place</strong>', script=True)
+#         popup = folium.Popup(test, max_width=2650)
+#         folium.RegularPolygonMarker(location=[latitude, longitude],
+#                                     popup=popup,
+#                                     tooltip=f"Click me ").add_to(m)
+#         folium.LatLngPopup().add_to(m)
+#         m = m._repr_html_()
+#         context['map'] = m
+#         return context
 
 
